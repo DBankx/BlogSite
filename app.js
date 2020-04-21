40242b8b45c3cbe86a1d8eba55bfd1d66b01d812
DBankx <61208493+DBankx@users.noreply.github.com>
2020-04-21 23:48:01 +0100

    implemented a database

--- app.js
+++ app.js
@@ -3,15 +3,48 @@ const _ = require('lodash')
 const bodyParser = require('body-parser')
 const app = express()
 const ejs = require('ejs')
+const mongoose = require('mongoose') 
 
-var posts = []
 
 app.set('view engine', 'ejs')
 app.use(express.static('public'))
 app.use(bodyParser.urlencoded({extended: true}))
 
+// create a mongoDB database 
+mongoose.connect('mongodb://localhost:27017/BlogDB', {useNewUrlParser: true, useUnifiedTopology: true})
+// create a new schema for the blogs
+const blogSchema = new mongoose.Schema({
+    name: String,
+    post: String
+})
+// create a new collection model for the DB
+const Blog = mongoose.model('Blog', blogSchema)
+// put some dummy data into the blog
+const blog1 = new Blog({
+    name: 'Blog 1',
+    post: 'This is the first dummy data for blog one in blog collections in the blog database'
+})
+
+const blog2 = new Blog({
+    name: 'Blog 2',
+    post: 'This is the second dummy data for blog one in blog collections in the blog database'
+})
+
+// put the blogs into an array to save
+const blogList = [blog1, blog2]
+
+// save the blog for the first time
+// Blog.insertMany(blogList, (err)=>{
+//     if (err){
+//         console.log(err)
+//     } else {
+//         console.log('items have been saved')
+//     }
+// })
+
 
 app.get('/', function(req, res){
+    // getting the date
         let today = new Date()
         let options = {
             weekday: 'long',
@@ -20,26 +53,64 @@ app.get('/', function(req, res){
             day: 'numeric'      
         }
         let date = today.toLocaleDateString('en-US', options)
-    res.render('index', {date:date, posts:posts})
+
+        // locating the blog name and posts in the DB
+        Blog.find((err, blogs)=>{
+         if (blogList.length == 0){
+             Blog.insertMany(blogList, (err)=>{
+                 if (err){
+                     console.log(err)
+                 } else {
+                     console.log('dummy blogs have been saved to the database')
+                 }
+             })
+             res.redirect('/')
+            //  rendering the home page
+         } else {
+            res.render('index', {date:date, posts:blogs})
+         }   
+        })
 })
 
 app.post('/', function(req, res){
-    var post = {
-        postTitle: req.body.title,
-        postText: req.body.text
-    }
-    posts.push(post)
+    // getting the data from the input boxes 
+    var blogTitle = req.body.title
+    var blogPost = req.body.text
+    
+    // parsing the data into the database
+    const newBlog = new Blog({
+        name: blogTitle,
+        post: blogPost
+    })
+    newBlog.save()
     res.redirect('/')
 
 })
 
+// creating a delete route 
+app.post('/delete', (req, res)=>{
+    var checkedId = req.body.check
+    // use the method below to remove the data from the id above from the database
+    Blog.findByIdAndRemove(checkedId, (err)=>{
+        if(err){
+            console.log(err)
+        } else {
+            console.log('Blog has been removed from the DB')
+        }
+        res.redirect('/')
+    })
+})
+
+// create a dynamic page for each blog
 app.get('/posts/:title', function(req, res){
     let nameofTitle = _.lowerCase(req.params.title)
-    posts.forEach(function(post){
-        if (_.lowerCase(post.postTitle) == nameofTitle) {
-            res.render('posts', {title:post.postTitle, body:post.postText})
-        } 
-    })
+    Blog.find((err, blogs)=>{
+        blogs.forEach((blog)=>{
+            if(_.lowerCase(blog.name) == nameofTitle){
+                res.render('posts', {title:blog.name, body:blog.post})
+            }
+        })
+    })   
 })
 
 
